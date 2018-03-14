64c37b23e5ddfd9a773ac31b4622d9753bf3a3ac
garretaserra <garretaserra@gmail.com>
2018-03-14 17:44:15 +0100

    Actualitzada clase create

--- src/Sesion.java
+++ src/Sesion.java
@@ -1,6 +1,22 @@
+import java.lang.reflect.Field;
+
 public class Sesion {
 
-    public void create(){}
+    public static void create(Class cls, Field... fields){
+        StringBuffer sb = new StringBuffer();
+        sb.append("INSERT INTO ");
+        sb.append(cls.getName());
+        sb.append(" (");
+        for(Field field : fields){
+            sb.append(field.getName());
+            sb.append(",");
+        }
+        sb.setLength(sb.length() - 1);  //Borrar la ulima coma
+        sb.append(") VALUES (");
+        for(Field field : fields){
+            //sb.append(field.get);
+        }
+    }
 
     public void read(){}
 
