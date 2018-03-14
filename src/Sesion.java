14e0a67633e9834cc2484e61b6e8d6ea56bcfafd
steph <stephypadilla08@gmail.com>
2018-03-14 17:58:49 +0100

    read

--- src/Sesion.java
+++ src/Sesion.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Field;
 
 public class Sesion {
 
-    public static void insert(Object obj){
+    public static void insert(Object obj){ //función create
         StringBuffer sb = new StringBuffer();
         sb.append("INSERT INTO ");
         sb.append(obj.getClass().getName());
@@ -21,7 +21,13 @@ public class Sesion {
         sb.append(");");
     }
 
-    public void read(){}
+    public void read(Class cls, String nombre){ //Te lo lee segun el nombre
+        StringBuffer sb = new StringBuffer(); //creo el buffer y empiezo a poner las instancias
+        sb.append("SELECT * FROM ");
+        sb.append(cls.getName());
+        sb.append(" WHERE id=");
+        sb.append(nombre);
+    }
 
     public void update(){}
 
