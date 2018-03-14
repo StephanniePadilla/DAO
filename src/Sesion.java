70fb7cff76e830471eb14f2065521323c0154816
garretaserra <garretaserra@gmail.com>
2018-03-14 18:45:21 +0100

    Actualitzada clase insert

--- src/Sesion.java
+++ src/Sesion.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Sesion {
@@ -26,7 +27,7 @@ public class Sesion {
 
     }
 
-    private static void getValue throws NoSuchMethodException (Object obj, String key) {
+    private static String getValue  (Object obj, String key) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException{
 
         Method m = obj.getClass().getDeclaredMethod("getName", null);
         Object o = m.invoke(obj, null);
@@ -34,6 +35,7 @@ public class Sesion {
             String s = (String )o;
             return s;
         }
+        return null;
     }
 
     public void read(Object obj,int id){ //Te lo lee segun el nombre
